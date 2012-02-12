e602d21b190a9b413a044d1c6f3beca58f13c1a3
Michael Adeyeye <micadeyeye@gmail.com>
2012-02-12 20:57:59 +0200

    removed some files

--- database/_connx.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$host='asmicom.easycgimysql.com';   // Hostname of MySQL server
-$dbUser='olutayo';    // Username for MySQL
-$dbPass='olu123tayo';    // Password for user
-$dbName='opencast'; // database name
-?>
